9cf3ba5d2befbe2794baa829b161fac437ff8015
FcoJavierGlez <FJGS_86@hotmail.com>
2019-10-08 20:59:01 +0200

    Corregido el ejercicio6.js, ahora valida el DNI al completo (incluyendo la letra)

--- Trimestre1/Ejercicio6/js/ejercicio6.js
+++ Trimestre1/Ejercicio6/js/ejercicio6.js
@@ -8,7 +8,17 @@
 let letras=['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N',
             'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
-let dni=Number.parseInt(prompt("Inserte su dni sin la letra"));
+let dni=prompt("Inserte su dni con la letra");
 
-if(dni>99999999 || dni<10000000 || !Number.isInteger(dni)) alert("Dni inválido.");
-else alert("Su dni es: "+dni+letras[dni%23]);
+
+if(dni.length==9) {
+    if((Number.parseInt(dni.substring(0,dni.length-1))>10000000 
+        && Number.parseInt(dni.substring(0,dni.length-1))<99999999) 
+        && dni.substring(8,dni.length)==letras[Number.parseInt(dni.substring(0,dni.length-1))%23]) {
+            alert("DNI correcto.");
+        }
+        else 
+            alert("Su dni correcto es: "+dni.substring(0,dni.length-1)+letras[Number.parseInt(dni.substring(0,dni.length-1))%23]);
+}
+else
+    alert("Su DNI es erróneo.");
